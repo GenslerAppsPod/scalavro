6334c20cd768b3a5c4c317c3a7268e8ddff6093d
Przemek Dyk <przemyslaw.dyk@smtsoftware.com>
2015-12-28 17:00:02 +0100

    refactorization

--- core/src/main/scala/com/gensler/scalavro/io/primitive/AvroBytesIO.scala
+++ core/src/main/scala/com/gensler/scalavro/io/primitive/AvroBytesIO.scala
@@ -1,16 +1,8 @@
 package com.gensler.scalavro.io.primitive
 
 import com.gensler.scalavro.types.primitive.AvroBytes
-import com.gensler.scalavro.error.{ AvroSerializationException, AvroDeserializationException }
-
-import org.apache.avro.io.{ BinaryEncoder, BinaryDecoder }
-
+import org.apache.avro.io.{ BinaryDecoder, BinaryEncoder }
 import spray.json._
-
-import scala.util.Try
-
-import java.nio.{ ByteBuffer, CharBuffer }
-import java.nio.charset.Charset
 
 object AvroBytesIO extends AvroBytesIO
 
@@ -24,37 +16,16 @@ trait AvroBytesIO extends AvroPrimitiveTypeIO[Seq[Byte]] {
 
   protected[scalavro] def write(
     bytes: Seq[Byte],
-    encoder: BinaryEncoder): Unit = encoder writeBytes bytes.toArray
+    encoder: BinaryEncoder): Unit = AvroByteArrayIO.write(bytes.toArray, encoder)
 
-  protected[scalavro] def read(decoder: BinaryDecoder) = {
-    val numBytes = decoder.readLong
-    val buffer = Array.ofDim[Byte](numBytes.toInt)
-    decoder.readFixed(buffer)
-    buffer.toIndexedSeq
-  }
+  protected[scalavro] def read(decoder: BinaryDecoder) = AvroByteArrayIO.read(decoder).toIndexedSeq
 
   ////////////////////////////////////////////////////////////////////////////
   // JSON ENCODING
   ////////////////////////////////////////////////////////////////////////////
 
-  val utf8: Charset = Charset.forName("UTF-8")
-  val utf8Encoder = utf8.newEncoder
+  def writePrimitiveJson(bytes: Seq[Byte]) = AvroByteArrayIO.writePrimitiveJson(bytes.toArray)
 
-  def writePrimitiveJson(bytes: Seq[Byte]) = {
-    val utf8String = new String(bytes.toArray, utf8)
-    JsString(utf8String)
-  }
-
-  def readJson(json: JsValue) = Try {
-    json match {
-      case JsString(value) => {
-        val byteBuf = utf8Encoder.encode(CharBuffer.wrap(value))
-        val bytes = new Array[Byte](byteBuf.remaining)
-        byteBuf.get(bytes)
-        bytes.toSeq
-      }
-      case _ => throw new AvroDeserializationException[Seq[Byte]]
-    }
-  }
+  def readJson(json: JsValue) = AvroByteArrayIO.readJson(json).map(_.toSeq)
 
 }
